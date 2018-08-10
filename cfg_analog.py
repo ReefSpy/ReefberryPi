f9bfa77e77d90bc76a77559591dc51dffea75614
ReefSpy <reefspy@gmail.com>
2018-08-10 14:50:37 -0400

    config changes

--- cfg_analog.py
+++ cfg_analog.py
@@ -1,6 +1,7 @@
 import tkinter as tk
 from tkinter import *
 from tkinter import ttk
+import cfg_common
 
 LARGE_FONT= ("Verdana", 12)
 
@@ -10,15 +11,25 @@ class PageAnalogProbes(tk.Frame):
         tk.Frame.__init__(self, parent)
 
         label = tk.Label(self, text="Analog Probes", font=LARGE_FONT)
-        label.pack(side=TOP, pady=10, anchor=W)
+        #label.pack(side=TOP, pady=10, anchor=W)
+        label.grid(row=0, column=0, pady=10, sticky=W)
+
+        # save button
+        self.saveimg=PhotoImage(file="images/save-blue-24.png")
+        self.btn_save = Button(self, text="Save", image=self.saveimg,
+                               compound='left', relief=RAISED, command=self.saveChanges)
+        self.btn_save.grid(row=1, column=0, sticky=W)
+
 
         # mcp3008 analog to digital converter
-        self.adcframe = LabelFrame(self, text="MCP3008 10-bit Analog to Digital Converter", relief=RAISED)
-        self.adcframe.pack(fill=X, side=TOP)
+        self.adcframe = LabelFrame(self, text="MCP3008 10-bit Analog to Digital Converter", relief=GROOVE)
+        #self.adcframe.pack(fill=X, side=TOP)
+        self.adcframe.grid(row=2, column=0, pady=10, sticky=W)
 
         # channel 0 frame
         self.adcch0frame = LabelFrame(self.adcframe, relief=SUNKEN, text="Channel 0")
-        self.adcch0frame.pack(fill=X, side=TOP)
+        #self.adcch0frame.pack(fill=X, side=TOP)
+        self.adcch0frame.grid(row=0, column=0, padx=10, pady=10)
 
         # channel 0 name
         self.ch0nameframe = LabelFrame(self.adcch0frame, relief=FLAT)
@@ -41,15 +52,17 @@ class PageAnalogProbes(tk.Frame):
         self.ch0sensortypemenu.pack(side=LEFT, anchor=W)
 
         # channel 0 enable checkbox
+        self.ch0Enabled = IntVar()
         self.ch0enableframe = LabelFrame(self.adcch0frame, relief=FLAT)
         self.ch0enableframe.pack(fill=X, side=TOP)
-        self.chk_ch0enable = Checkbutton(self.ch0enableframe,text="Enable")
+        self.chk_ch0enable = Checkbutton(self.ch0enableframe,text="Enable", variable=self.ch0Enabled)
         self.chk_ch0enable.pack(side=LEFT, anchor=W, padx=20)
 
 
         # channel 1 frame
         self.adcch1frame = LabelFrame(self.adcframe, relief=SUNKEN, text="Channel 1")
-        self.adcch1frame.pack(fill=X, side=TOP)
+        #self.adcch1frame.pack(fill=X, side=TOP)
+        self.adcch1frame.grid(row=0, column=1, padx=10, pady=10)
 
         # channel 1 name
         self.ch1nameframe = LabelFrame(self.adcch1frame, relief=FLAT)
@@ -72,7 +85,81 @@ class PageAnalogProbes(tk.Frame):
         self.ch1sensortypemenu.pack(side=LEFT, anchor=W)
 
         # channel 1 enable checkbox
+        self.ch1Enabled = IntVar()
         self.ch1enableframe = LabelFrame(self.adcch1frame, relief=FLAT)
         self.ch1enableframe.pack(fill=X, side=TOP)
-        self.chk_ch1enable = Checkbutton(self.ch1enableframe,text="Enable")
+        self.chk_ch1enable = Checkbutton(self.ch1enableframe,text="Enable", variable=self.ch1Enabled)
         self.chk_ch1enable.pack(side=LEFT, anchor=W, padx=20)
+
+        # read values from config file
+        # Ch0 name
+        val = cfg_common.readINIfile("mcp3008", "ch0_name", "Unnamed")
+        self.txt_ch0name.delete(0,END)
+        self.txt_ch0name.insert(0,val)
+        # Ch0 enabled
+        val = cfg_common.readINIfile("mcp3008", "ch0_enabled", "False")
+        if str(val) == "True":
+            self.chk_ch0enable.select()
+        else:
+            self.chk_ch0enable.deselect()
+        # Ch0 type
+        val = cfg_common.readINIfile("mcp3008", "ch0_type", "pH")
+        self.ch0sensortype.set(val)
+        
+
+        # Ch1 name
+        val = cfg_common.readINIfile("mcp3008", "ch1_name", "Unnamed")
+        self.txt_ch1name.delete(0,END)
+        self.txt_ch1name.insert(0,val)
+        #Ch1 enabled
+        val = cfg_common.readINIfile("mcp3008", "ch1_enabled", "False")
+        if str(val) == "True":
+            self.chk_ch1enable.select()
+        else:
+            self.chk_ch1enable.deselect()
+        # Ch1 type
+        val = cfg_common.readINIfile("mcp3008", "ch1_type", "pH")
+        self.ch1sensortype.set(val)
+
+    def saveChanges(self):
+        # Channel 0
+        if cfg_common.writeINIfile('mcp3008', 'ch0_name', str(self.txt_ch0name.get())) != True:
+             messagebox.showerror("Analog Settings",
+                                 "Error: Could not save changes! \nNew configuration not saved.")
+        
+        if self.ch0Enabled.get() == True:
+            chkstate = "True"
+        else:
+            chkstate = "False"
+            
+        if cfg_common.writeINIfile('mcp3008', 'ch0_enabled', str(chkstate)) != True:
+           messagebox.showerror("Analog Settings",
+                                 "Error: Could not save changes! \nNew configuration not saved.")
+        
+        if cfg_common.writeINIfile('mcp3008', 'ch0_type', str(self.ch0sensortype.get())) != True:
+           messagebox.showerror("Analog Settings",
+                                 "Error: Could not save changes! \nNew configuration not saved.")
+           
+        # Channel 1
+        if cfg_common.writeINIfile('mcp3008', 'ch1_name', str(self.txt_ch1name.get())) != True:
+             messagebox.showerror("Analog Settings",
+                                 "Error: Could not save changes! \nNew configuration not saved.")
+
+        if self.ch1Enabled.get() == True:
+            chkstate = "True"
+        else:
+            chkstate = "False"
+            
+        if cfg_common.writeINIfile('mcp3008', 'ch1_enabled', str(chkstate)) != True:
+           messagebox.showerror("Analog Settings",
+                                 "Error: Could not save changes! \nNew configuration not saved.")
+
+        if cfg_common.writeINIfile('mcp3008', 'ch1_type', str(self.ch1sensortype.get())) != True:
+           messagebox.showerror("Analog Settings",
+                                 "Error: Could not save changes! \nNew configuration not saved.")
+        
+
+
+
+        
+        
--- cfg_environmental.py
+++ cfg_environmental.py
@@ -1,6 +1,7 @@
 import tkinter as tk
 from tkinter import *
 from tkinter import ttk
+import cfg_common
 
 LARGE_FONT= ("Verdana", 12)
 
@@ -10,37 +11,74 @@ class PageEnvironmental(tk.Frame):
         tk.Frame.__init__(self, parent)
 
         label = tk.Label(self, text="Environmental Sensors", font=LARGE_FONT)
-        label.pack(side=TOP, pady=10, anchor=W)
+        label.grid(row=0, column=0, pady=10, sticky=W)
+
+        # top frame for toolbar
+        #self.frame_toolbar = LabelFrame(self, relief= FLAT)
+        #self.frame_toolbar.pack(fill=X, side=TOP)
+        
+        # save button
+        self.saveimg=PhotoImage(file="images/save-blue-24.png")
+        self.btn_save = Button(self, text="Save", image=self.saveimg,
+                               compound='left', relief=RAISED, command=self.saveChanges)
+        self.btn_save.grid(row=1, column=0, sticky=W)
 
         # dht11/22 
-        self.dhtframe = LabelFrame(self, text="DHT11/22 Temperature and Humidity Sensor", relief=RAISED)
-        self.dhtframe.pack(fill=X, side=TOP)
-        # temperature
-        self.dhttempframe = LabelFrame(self.dhtframe, relief=FLAT)
-        self.dhttempframe.pack(fill=X, side=TOP)
-        self.lbl_dhttemp = Label(self.dhttempframe,text="Temperature Sensor Name:")
-        self.lbl_dhttemp.pack(side=LEFT, anchor=W)
-        self.txt_dhttemp = Entry(self.dhttempframe)
-        self.txt_dhttemp.pack(side=LEFT, anchor=W)
-        self.dhttempenableframe = LabelFrame(self.dhtframe, relief=FLAT)
-        self.dhttempenableframe.pack(fill=X, side=TOP)
-        self.chk_dhttemp = Checkbutton(self.dhttempenableframe,text="Enable Temperature Sensor")
-        self.chk_dhttemp.pack(side=LEFT, anchor=W, padx=20)
+        self.dhtframe = LabelFrame(self, text="DHT11/22 Temperature and Humidity Sensor", relief=GROOVE)
+        self.dhtframe.grid(row=2, column=0, pady=10)
 
-        # spacer
-        self.dhtspacer = LabelFrame(self.dhtframe, relief=FLAT)
-        self.dhtspacer.pack(side=TOP, anchor=W, pady=5)
+        # enable sensor
+        self.dhtEnabled = IntVar()
+        self.chk_dhtenable = Checkbutton(self.dhtframe,text="Enable Sensor", variable=self.dhtEnabled)
+        self.chk_dhtenable.grid(row=0, column=0, sticky=W, padx=5, pady=5)
+        
+        # temperature
+        self.lbl_dhttemp = Label(self.dhtframe,text="Temperature Sensor Name:")
+        self.lbl_dhttemp.grid(row=1, column=0, sticky=E, padx=10, pady=5)
+        self.txt_dhttemp = Entry(self.dhtframe)
+        self.txt_dhttemp.grid(row=1, column=1, padx=10, pady=5)
 
         # humidity
-        self.dhthumframe = LabelFrame(self.dhtframe, relief=FLAT)
-        self.dhthumframe.pack(fill=X, side=TOP)
-        self.lbl_dhthum = Label(self.dhthumframe,text="Humidity Sensor Name:")
-        self.lbl_dhthum.pack(side=LEFT, anchor=W)
-        self.txt_dhthum = Entry(self.dhthumframe)
-        self.txt_dhthum.pack(side=LEFT, anchor=W)
+        self.lbl_dhthum = Label(self.dhtframe,text="Humidity Sensor Name:")
+        self.lbl_dhthum.grid(row=2, column=0, sticky=E, padx=10, pady=5)
+        self.txt_dhthum = Entry(self.dhtframe)
+        self.txt_dhthum.grid(row=2, column=1, padx=10, pady=5)
+
+        # read values from config file
+        enabledht = cfg_common.readINIfile("dht11/22", "enabled", "False")
+        if str(enabledht) == "True":
+            self.chk_dhtenable.select()
+        else:
+            self.chk_dhtenable.deselect()
+
+        tempname = cfg_common.readINIfile("dht11/22", "temperature_name", "Ambient Temperature")
+        self.txt_dhttemp.delete(0,END)
+        self.txt_dhttemp.insert(0,tempname)
+       
+        humname = cfg_common.readINIfile("dht11/22", "humidity_name", "Humidity")
+        self.txt_dhthum.delete(0,END)
+        self.txt_dhthum.insert(0,humname)
         
-        self.dhthumenableframe = LabelFrame(self.dhtframe, relief=FLAT)
-        self.dhthumenableframe.pack(fill=X, side=TOP)
-        self.chk_dhthum = Checkbutton(self.dhthumenableframe,text="Enable Humidity Sensor")
-        self.chk_dhthum.pack(side=LEFT, anchor=W, padx=20)
+    def saveChanges(self):
+        # enabled setting
+        if self.dhtEnabled.get() == True:
+            chkstate = "True"
+        else:
+            chkstate = "False"
+            
+        if cfg_common.writeINIfile('dht11/22', 'enabled', str(chkstate)) != True:
+           messagebox.showerror("Environmental Settings",
+                                 "Error: Could not save changes! \nNew configuration not saved.")
+        # temperature name
+        if cfg_common.writeINIfile('dht11/22', 'temperature_name', str(self.txt_dhttemp.get())) != True:
+             messagebox.showerror("Environmental Settings",
+                                 "Error: Could not save changes! \nNew configuration not saved.")
+            
+        # humidity name 
+        if cfg_common.writeINIfile('dht11/22', 'humidity_name', str(self.txt_dhthum.get())) != True:
+            messagebox.showerror("Environmental Settings",
+                                 "Error: Could not save changes! \nNew configuration not saved.")
         
+        
+        
+
