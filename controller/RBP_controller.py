dee043ebfe8848abf57f0f9ce19aee27ae1dea41
ReefSpy <reefspy@gmail.com>
2024-01-21 14:58:31 -0500

    enable skimmer api

--- controller/RBP_controller.py
+++ controller/RBP_controller.py
@@ -342,14 +342,23 @@ def get_outlet_list():
                                 "light_on": AppPrefs.outletDict[outlet].light_on,
                                 "light_off": AppPrefs.outletDict[outlet].light_off,
                                 "always_state": AppPrefs.outletDict[outlet].always_state,
-                                "return_enable_feed_a": AppPrefs.outletDict[outlet].return_enable_feed_a,
-                                "return_enable_feed_b": AppPrefs.outletDict[outlet].return_enable_feed_b,
-                                "return_enable_feed_c": AppPrefs.outletDict[outlet].return_enable_feed_c,
-                                "return_enable_feed_d": AppPrefs.outletDict[outlet].return_enable_feed_d,
+                                "return_enable_feed_a": (AppPrefs.outletDict[outlet].return_enable_feed_a).lower() == "true",
+                                "return_enable_feed_b": (AppPrefs.outletDict[outlet].return_enable_feed_b).lower() == "true",
+                                "return_enable_feed_c": (AppPrefs.outletDict[outlet].return_enable_feed_c).lower() == "true",
+                                "return_enable_feed_d": (AppPrefs.outletDict[outlet].return_enable_feed_d).lower() == "true",
                                 "return_feed_delay_a": AppPrefs.outletDict[outlet].return_feed_delay_a,
                                 "return_feed_delay_b": AppPrefs.outletDict[outlet].return_feed_delay_b,
                                 "return_feed_delay_c": AppPrefs.outletDict[outlet].return_feed_delay_c,
                                 "return_feed_delay_d": AppPrefs.outletDict[outlet].return_feed_delay_d,
+
+                                "skimmer_enable_feed_a": (AppPrefs.outletDict[outlet].skimmer_enable_feed_a).lower() == "true",
+                                "skimmer_enable_feed_b": (AppPrefs.outletDict[outlet].skimmer_enable_feed_b).lower() == "true",
+                                "skimmer_enable_feed_c": (AppPrefs.outletDict[outlet].skimmer_enable_feed_c).lower() == "true",
+                                "skimmer_enable_feed_d": (AppPrefs.outletDict[outlet].skimmer_enable_feed_d).lower() == "true",
+                                "skimmer_feed_delay_a": AppPrefs.outletDict[outlet].skimmer_feed_delay_a,
+                                "skimmer_feed_delay_b": AppPrefs.outletDict[outlet].skimmer_feed_delay_b,
+                                "skimmer_feed_delay_c": AppPrefs.outletDict[outlet].skimmer_feed_delay_c,
+                                "skimmer_feed_delay_d": AppPrefs.outletDict[outlet].skimmer_feed_delay_d,
 
                                 }
         if len(outletdict) < 8:
@@ -782,6 +791,86 @@ def set_outlet_params_return(outletid):
     
     except Exception as e:
         AppPrefs.logger.error("set_outlet_params_return: " +  str(e))
+        response = jsonify({"msg": str(e)})
+        response.status_code = 500 
+        return response
+
+#####################################################################
+# set_outlet_params_skimmer/<outletid>
+# set the paramters for outlet of type: Skimmer
+# must specify outletid and deliver payload in json
+#####################################################################     
+
+@app.route('/set_outlet_params_skimmer/<outletid>' , methods=["PUT", "POST"])
+@cross_origin()
+def set_outlet_params_skimmer(outletid):
+    global logger
+
+    try:
+        global AppPrefs
+
+        print(outletid)
+        response = {}
+        payload = request.get_json()
+        print(payload)
+        skimmer_enable_feed_a = payload["skimmer_enable_feed_a"]
+        skimmer_enable_feed_b = payload["skimmer_enable_feed_b"]
+        skimmer_enable_feed_c = payload["skimmer_enable_feed_c"]
+        skimmer_enable_feed_d = payload["skimmer_enable_feed_d"]
+        skimmer_feed_delay_a = payload["skimmer_feed_delay_a"]
+        skimmer_feed_delay_b = payload["skimmer_feed_delay_b"]
+        skimmer_feed_delay_c = payload["skimmer_feed_delay_c"]
+        skimmer_feed_delay_d = payload["skimmer_feed_delay_d"]
+        outletname = payload["outletname"]
+        control_type = payload["control_type"]
+
+        response = jsonify({"msg": 'Updated outlet data for type: Skimmer',
+                            "outletid": outletid,
+                            "outletname": outletname,
+                            "control_type": control_type,
+                            "skimmer_enable_feed_a": skimmer_enable_feed_a,
+                            "skimmer_enable_feed_b": skimmer_enable_feed_b,
+                            "skimmer_enable_feed_c": skimmer_enable_feed_c,
+                            "skimmer_enable_feed_d": skimmer_enable_feed_d,
+                            "skimmer_feed_delay_a": skimmer_feed_delay_a,
+                            "skimmer_feed_delay_b": skimmer_feed_delay_b,
+                            "skimmer_feed_delay_c": skimmer_feed_delay_c,
+                            "skimmer_feed_delay_d": skimmer_feed_delay_d,
+                            })
+
+        response.status_code = 200      
+
+        # build table object from table in DB
+        metadata_obj = MetaData()
+        outlet_table = Table("outlets", metadata_obj, autoload_with=sqlengine)
+        
+        stmt = (
+            update(outlet_table)
+            .where(outlet_table.c.outletid == outletid)
+            .where(outlet_table.c.appuid == AppPrefs.appuid)
+            .values(outletname=outletname, 
+                    control_type=control_type, 
+                    skimmer_enable_feed_a=skimmer_enable_feed_a, 
+                    skimmer_enable_feed_b=skimmer_enable_feed_b,
+                    skimmer_enable_feed_c=skimmer_enable_feed_c,
+                    skimmer_enable_feed_d=skimmer_enable_feed_d,
+                    skimmer_feed_delay_a=skimmer_feed_delay_a,
+                    skimmer_feed_delay_b=skimmer_feed_delay_b,
+                    skimmer_feed_delay_c=skimmer_feed_delay_c,
+                    skimmer_feed_delay_d=skimmer_feed_delay_d,
+                        )
+            )
+
+        with sqlengine.connect() as conn:
+            result = conn.execute(stmt)
+            conn.commit()
+
+        defs_mysql.readOutletPrefs_ex(sqlengine, AppPrefs, logger)
+
+        return response
+    
+    except Exception as e:
+        AppPrefs.logger.error("set_outlet_params_skimmer: " +  str(e))
         response = jsonify({"msg": str(e)})
         response.status_code = 500 
         return response
