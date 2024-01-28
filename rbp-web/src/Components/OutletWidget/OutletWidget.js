1be6c52b794da97f724eab288bab592bb6f00287
ReefSpy <33102654+ReefSpy@users.noreply.github.com>
2024-01-28 16:57:01 -0500

    enable drag and drop

--- rbp-web/src/Components/OutletWidget/OutletWidget.js
+++ rbp-web/src/Components/OutletWidget/OutletWidget.js
@@ -1,7 +1,7 @@
 import React, { Component } from "react";
 import MultiToggle from "react-multi-toggle";
 import "./togglestyle.css";
-import "./OutletWidget.css"
+import "./OutletWidget.css";
 import cogicon from "./cog.svg";
 import OutletWidgetModal from "./OutletWidgetModal";
 
@@ -40,84 +40,151 @@ export class OutletWidget extends Component {
     console.log(this.props.data);
 
     this.setState({ buttonstateidx: val });
-    //this.props.onOutletWidgetClick(val, outletid);
-    this.props.onButtonStateChange(outletid, val);
+
+    // console.log("I'm handling the button click " + outletid + " " + val);
+    const newOutLetData = { ...this.state.OutletData };
+    newOutLetData.ischanged = true;
+    this.setState({ OutletData: newOutLetData });
+
+    let apiURL = process.env.REACT_APP_API_PUT_OUTLET_BUTTONSTATE.concat(
+      outletid
+    )
+      .concat("/")
+      .concat(val);
+    //console.log(apiURL)
+    this.apiCall(apiURL, this.handleOutletButtonClick);
   }
+
+  handleOutletButtonClick(retData) {
+    console.log(retData);
+  }
+
+  // handleOutletButtonClick(outletid, buttonval) {
+  //   console.log("I'm handling the button click " + outletid + " " + buttonval);
+  //   var outletListArrayClone = this.state.OutletArray.slice(0);
+  //   for (var outletClone in outletListArrayClone) {
+  //     if (outletListArrayClone[outletClone]["outletid"] === outletid) {
+  //       console.log("Found a match");
+  //       // console.log(outletdata[outlet].outletid);
+  //       // console.log(outletListArrayClone[outletClone]["outletid"]);
+  //       outletListArrayClone[outletClone]["button_state"] = buttonval;
+  //       outletListArrayClone[outletClone]["ischanged"] = true;
+  //     }
+  //   }
+  //   //console.log(this.state.OutletArray)
+  //   this.setState({ OutletArray: outletListArrayClone });
+  //   console.log(this.state.OutletArray);
+
+  //   let apiURL = process.env.REACT_APP_API_PUT_OUTLET_BUTTONSTATE.concat(
+  //     outletid
+  //   )
+  //     .concat("/")
+  //     .concat(buttonval);
+  //   //console.log(apiURL)
+  //   this.apiCall(apiURL);
+  // }
+
   componentDidMount() {
     this.setState({ buttonstateidx: this.props.data.button_state });
+
+    // update stats
+    let ApiGetStats = process.env.REACT_APP_API_GET_CURRENT_OUTLET_STATS.concat(
+      this.props.data.outletid
+    );
+    this.apiCall(ApiGetStats, this.SetOutletData);
+
+    this.interval2 = setInterval(() => {
+      this.apiCall(ApiGetStats, this.SetOutletData);
+    }, 2000);
   }
 
-///////
-handleOpenOutletPrefsModal = () => {
-  this.setState({ setOutletPrefsModalOpen: true });
-  this.setState({ isOutletPrefsModalOpen: true });
-};
+  componentWillUnmount() {
+    clearInterval(this.interval);
+    clearInterval(this.interval2);
+  }
 
-handleCloseOutletPrefsModal = () => {
-  this.setState({ setOutletPrefsModalOpen: false });
-  this.setState({ isOutletPrefsModalOpen: false });
+  handleOpenOutletPrefsModal = () => {
+    this.setState({ setOutletPrefsModalOpen: true });
+    this.setState({ isOutletPrefsModalOpen: true });
+  };
 
-};
+  handleCloseOutletPrefsModal = () => {
+    this.setState({ setOutletPrefsModalOpen: false });
+    this.setState({ isOutletPrefsModalOpen: false });
+  };
 
-handleOutletPrefsFormSubmit = (data) => {
- // this.setState({ setOutletPrefsFormData: data });
-  this.handleCloseOutletPrefsModal();
-  console.log(data)
+  handleOutletPrefsFormSubmit = (data) => {
+    this.handleCloseOutletPrefsModal();
+    console.log(data);
+  };
 
-  // let updateApiURL = "http://xpi01.local:5000/set_probe_name/"
-  //   .concat(data.probeid)
-  //   .concat("/")
-  //   .concat(data.probename);
-  // this.apiCall(updateApiURL, );
-};
-//////
+  SetOutletData = (data) => {
+    this.setState({ OutletName: data.outletname });
+    this.setState({ OutletStatus: data.outletstatus });
+    this.setState({ OutletData: data });
+    this.setState({ buttonstateidx: data.button_state });
+  };
 
+  // generic API call structure
+  apiCall(endpoint, callback) {
+    fetch(endpoint)
+      .then((response) => {
+        if (!response.ok) {
+          if (response.status === 404) {
+            throw new Error("Data not found");
+          } else if (response.status === 500) {
+            throw new Error("Server error");
+          } else {
+            throw new Error("Network response was not ok");
+          }
+        }
+        return response.json();
+      })
+      .then((data) => {
+        callback(data);
+      })
+      .catch((error) => {
+        console.error("Error:", error);
+      });
+  }
 
   render() {
-    // const widgetstyle = {
-    //   color: "black",
-    //   backgroundColor: "white",
-    //   padding: "10px",
-    //   border: "1px solid black",
-    //   width: "300px",
-    //   display: "block",
-    // };
-
-
     return (
       <div class="outletcontainer">
-        <div class="outletitem outletname">{this.props.data.outletname}</div>
-        <div class = "outletitem multitoggle">
+        <div class="outletitem outletname">{this.state.OutletName}</div>
+        <div class="outletitem multitoggle">
           <MultiToggle
             options={groupOptions}
-            label={this.props.data.outletstatus}
+            label={this.state.OutletStatus}
             onSelectOption={this.onToggleSelect}
-            selectedOption={this.props.data.button_state}
-            //selectedOption={this.state.buttonstateidx}
+            selectedOption={this.state.buttonstateidx}
             className={"outletSlider"}
           />
         </div>
         <div class="outletitem outletseticon">
-          <button class = "outletsetbtn"><img
-            src={cogicon}
-            alt="settings"
-            height="14"
-            width="14"
-            onClick={this.handleOpenOutletPrefsModal}
-          /></button>
+          <button class="outletsetbtn">
+            <img
+              src={cogicon}
+              alt="settings"
+              height="14"
+              width="14"
+              onClick={this.handleOpenOutletPrefsModal}
+            />
+          </button>
         </div>
 
-        {(this.state.isOutletPrefsModalOpen) ? 
-        <OutletWidgetModal
-        isOpen={this.state.isOutletPrefsModalOpen}
-        onSubmit={this.handleOutletPrefsFormSubmit}
-        onClose={this.handleCloseOutletPrefsModal}
-        OutletName={this.props.data.outletname}
-        OutletID={this.props.data.outletid}
-        ControlType={this.props.data.control_type}
-        data={this.props.data}
-        probearray={this.props.probearray}
-        /> : null}
+        {this.state.isOutletPrefsModalOpen ? (
+          <OutletWidgetModal
+            isOpen={this.state.isOutletPrefsModalOpen}
+            onSubmit={this.handleOutletPrefsFormSubmit}
+            onClose={this.handleCloseOutletPrefsModal}
+            OutletName={this.state.OutletName}
+            OutletID={this.props.data.outletid}
+            ControlType={this.props.data.control_type}
+            data={this.state.OutletData}
+            probearray={this.props.probearray}
+          />
+        ) : null}
       </div>
     );
   }
