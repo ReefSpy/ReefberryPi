c096eec8878aa1d115fe66a921a075ebbc719752
ReefSpy <33102654+ReefSpy@users.noreply.github.com>
2024-03-16 14:16:20 -0400

    Analytics Dropdown lists are Incorrect #8

--- rbp-web/src/Components/ProbeWidget/ProbeWidget.jsx
+++ rbp-web/src/Components/ProbeWidget/ProbeWidget.jsx
@@ -110,6 +110,8 @@ export class ProbeWidget extends Component {
   }
 
   GetChartData = (chartdata) => {
+
+  // need to convert timestamp to milliseconds to show up properly in HighCharts
     let valueArray1 = [];
     for (let datapoint in chartdata) {
       let newDate = new Date(chartdata[datapoint][0]).getTime();
